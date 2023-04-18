1d9eb1b4e85bb2c8d71a01c11f8fc2d1c2c46639
thangdev03 <thangdev03@gmail.com>
2023-04-18 21:06:05 +0700

    add product item js

---
+++ assets/main.js
@@ -0,0 +1,42 @@
+const shirtContainer = document.getElementById("shirt-container");
+const shirtItems = [
+    {
+        image: './assets/img/Tshirt01.jpg',
+        name: 'Áo Marvel Big Logo',    
+        price: '299.000đ',
+        desc: 'Áo thun, oversize, màu đen, size M/L/XL',
+    },
+    {
+        image: './assets/img/Tshirt02.jpg',
+        name: 'Áo thun Marvel Captain America',    
+        price: '299.000đ',
+        desc: 'Áo thun, oversize, màu xanh than, size M/L/XL',
+    },
+    {
+        image: './assets/img/Tshirt03.jpg',
+        name: 'Áo thun Marvel Big Logo Hulk',    
+        price: '299.000đ',
+        desc: 'Áo thun, oversize, màu xanh lam, size M/L/XL',
+    },
+    {
+        image: './assets/img/Tshirt04.jpg',
+        name: 'Áo thun Marvel Thor Mjolnir',    
+        price: '299.000đ',
+        desc: 'Áo thun, oversize, màu trắng, size M/L/XL',
+    }
+]
+ 
+for (const shirtItem of shirtItems) {
+    const shirtItemDiv = document.createElement('div');
+    shirtItemDiv.classList.add('shirt-item');
+    shirtItemDiv.innerHTML = `
+        <img class="shirt-item__img" src="${shirtItem.image}" alt="Áo Marvel Big Logo">
+        <h1 class="shirt-item__name">${shirtItem.name}</h1>
+        <p class="shirt-item__price">${shirtItem.price}</p>
+        <p class="shirt-item__desc">${shirtItem.desc}</p>
+        <button class="shirt-item__buyBtn">Mua hàng</button>
+    `;
+    shirtContainer.appendChild(shirtItemDiv);
+}
+
+
